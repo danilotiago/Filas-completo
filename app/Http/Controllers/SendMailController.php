eb8fd6899457eaccdbc1ca5bccfb4ecad3676975
Danilo Tiago <programacao.desenvolvimento@gmail.com>
2018-07-21 15:37:27 -0300

    implementando metodo enviEmail

--- app/Http/Controllers/SendMailController.php
+++ app/Http/Controllers/SendMailController.php
@@ -2,9 +2,13 @@
 
 namespace App\Http\Controllers;
 
+use App\Jobs\SendMailJob;
 use Illuminate\Http\Request;
 
 class SendMailController extends Controller
 {
-    //
+    public function enviaEmail()
+    {
+        $this->dispatch(new SendMailJob());
+    }
 }
